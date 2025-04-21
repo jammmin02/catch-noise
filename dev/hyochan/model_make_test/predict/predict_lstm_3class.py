cd3c7a7474a6953d022e552d82c972a192b5f3c2
HyoChan Kim <skkhc0317@gmail.com>
2025-04-21 15:00:16 +0900

    25.04.20. 잘못된 학습 사례 (#30)
    * 25.04.15. Branch test
    
    * 25.04.18. Update development environment and learning model development
    
    * 25.04.18. Modify preprocess file
    
    * 25.04.18. Add audio_trimming file & Update model make code
    
    * 25.04.18. Modify code
    
    * 25.04.18. Modify audio_trimming file
    
    * 25.04.18. Modify audio_trimming flie
    
    * 25.04.20. Modify preprocessing method
    
    * 25.04.20. Change segment_duration

--- dev/hyochan/model_make_test/predict/predict_lstm_3class.py
+++ dev/hyochan/model_make_test/predict/predict_lstm_3class.py
@@ -13,7 +13,7 @@ from tensorflow.keras.models import load_model
 sr = 22050
 n_mfcc = 13
 hop_length = 512
-segment_duration = 3.0
+segment_duration = 2.0
 frame_per_second = sr / hop_length
 max_len = int(frame_per_second * segment_duration)
 
@@ -21,8 +21,8 @@ model_path = "hyochan/model_make_test/dataset/outputs/cnn_lstm/cnn_lstm_model.h5
 test_folder = "hyochan/model_make_test/test_audio_batch"
 save_dir = os.path.join(test_folder, "visuals")
 
-class_names = ['silent', 'neutral', 'noisy']
-class_colors = {'silent': 'skyblue', 'neutral': 'orange', 'noisy': 'tomato'}
+class_names = ['non_noisy', 'neutral', 'noisy']
+class_colors = {'non_noisy': 'skyblue', 'neutral': 'orange', 'noisy': 'tomato'}
 
 # 📥 Load model
 model = load_model(model_path)
